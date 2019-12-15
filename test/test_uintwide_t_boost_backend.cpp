408e0a6caa777b045e551f043d4ccb286e01182c
Christopher Kormanyos <ckormanyos@yahoo.com>
2019-12-15 23:09:26 +0100

    Add missing copyright text.

--- test/test_uintwide_t_boost_backend.cpp
+++ test/test_uintwide_t_boost_backend.cpp
@@ -1,5 +1,9 @@
-#include <iomanip>
-#include <iostream>
+///////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2019.                        //
+//  Distributed under the Boost Software License,                //
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt          //
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)             //
+///////////////////////////////////////////////////////////////////
 
 #include <boost/multiprecision/uintwide_t_backend.hpp>
 
