ba1990d8552e1c815a03c8c84717de03fcf93928
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-03-24 20:17:50 +0100

    Correct and clean some template/function syntax

--- math/wide_integer/uintwide_t.h
+++ math/wide_integer/uintwide_t.h
@@ -1452,40 +1452,43 @@
                                            const limb_type*         b,
                                            const std::uint_fast32_t count)
     {
-      std::int_fast8_t cmp_result;
+      using local_const_reverse_iterator_type = std::reverse_iterator<const limb_type*>;
 
-      std::int_fast32_t element_index = std::int_fast32_t(count) - 1;
+      local_const_reverse_iterator_type rcbegin_a(a + count);
+      local_const_reverse_iterator_type rcend_a  (a);
+      local_const_reverse_iterator_type rcbegin_b(b + count);
+      local_const_reverse_iterator_type rcend_b  (b);
 
-      while((   (element_index >= 0)
-             && (a[(element_index)] == b[(element_index)])))
+      const auto mismatch_pair = std::mismatch(rcbegin_a, rcend_a, rcbegin_b);
+
+      std::int_fast8_t n_return;
+
+      if((mismatch_pair.first != rcend_a) || (mismatch_pair.second != rcend_b))
       {
-        --element_index;
-      }
+        const limb_type left  = *mismatch_pair.first;
+        const limb_type right = *mismatch_pair.second;
 
-      if(element_index == std::int_fast32_t(-1))
-      {
-        cmp_result = std::int_fast8_t(0);
+        n_return = ((left > right) ? static_cast<std::int_fast8_t>( 1)
+                                   : static_cast<std::int_fast8_t>(-1));
       }
       else
       {
-        const bool left_is_greater_than_right = (a[element_index] > b[element_index]);
-
-        cmp_result = (left_is_greater_than_right ? std::int_fast8_t(1) : std::int_fast8_t(-1));
+        n_return = static_cast<std::int_fast8_t>(0);
       }
 
-      return cmp_result;
+      return n_return;
     }
 
     template<const std::uint_fast32_t OtherDigits2>
-    static void eval_mul_unary(      uintwide_t<OtherDigits2, LimbType>& u,
-                               const uintwide_t<OtherDigits2, LimbType>& v,
-                               typename std::enable_if<((OtherDigits2 / std::numeric_limits<LimbType>::digits) < uintwide_t::number_of_limbs_karatsuba_threshold)>::type* = nullptr)
+    static void eval_mul_unary(      uintwide_t<OtherDigits2, LimbType, AllocatorType>& u,
+                               const uintwide_t<OtherDigits2, LimbType, AllocatorType>& v,
+                               typename std::enable_if<((OtherDigits2 / std::numeric_limits<LimbType>::digits) < number_of_limbs_karatsuba_threshold)>::type* = nullptr)
     {
       // Unary multiplication function using schoolbook multiplication,
       // but we only need to retain the low half of the n*n algorithm.
       // In other words, this is an n*n->n bit multiplication.
 
-      constexpr std::uint_fast32_t local_number_of_limbs = uintwide_t<OtherDigits2, LimbType>::number_of_limbs;
+      constexpr std::uint_fast32_t local_number_of_limbs = uintwide_t<OtherDigits2, LimbType, AllocatorType>::number_of_limbs;
 
       representation_type result;
 
@@ -1502,20 +1505,20 @@
     template<const std::uint_fast32_t OtherDigits2>
     static void eval_mul_unary(      uintwide_t<OtherDigits2, LimbType, AllocatorType>& u,
                                const uintwide_t<OtherDigits2, LimbType, AllocatorType>& v,
-                               typename std::enable_if<((OtherDigits2 / std::numeric_limits<LimbType>::digits) >= uintwide_t::number_of_limbs_karatsuba_threshold)>::type* = nullptr)
+                               typename std::enable_if<((OtherDigits2 / std::numeric_limits<LimbType>::digits) >= number_of_limbs_karatsuba_threshold)>::type* = nullptr)
     {
       // Unary multiplication function using Karatsuba multiplication.
 
-      constexpr std::uint_fast32_t local_number_of_limbs = uintwide_t<OtherDigits2, LimbType>::number_of_limbs;
+      constexpr std::uint_fast32_t local_number_of_limbs = uintwide_t<OtherDigits2, LimbType, AllocatorType>::number_of_limbs;
 
       // TBD: Can use specialized allocator or memory pool for these arrays.
       typename std::conditional<std::is_same<AllocatorType, void>::value,
-                                detail::fixed_static_array <limb_type, number_of_limbs * 2U>,
-                                detail::fixed_dynamic_array<limb_type, number_of_limbs * 2U, AllocatorType>>::type result;
+                                detail::fixed_static_array <limb_type, local_number_of_limbs * 2U>,
+                                detail::fixed_dynamic_array<limb_type, local_number_of_limbs * 2U, AllocatorType>>::type result;
 
       typename std::conditional<std::is_same<AllocatorType, void>::value,
-                                detail::fixed_static_array <limb_type, number_of_limbs * 4U>,
-                                detail::fixed_dynamic_array<limb_type, number_of_limbs * 4U, AllocatorType>>::type t;
+                                detail::fixed_static_array <limb_type, local_number_of_limbs * 4U>,
+                                detail::fixed_dynamic_array<limb_type, local_number_of_limbs * 4U, AllocatorType>>::type t;
 
       eval_multiply_kara_n_by_n_to_2n(result.data(),
                                       u.values.data(),
@@ -1528,11 +1531,11 @@
                 u.values.begin());
     }
 
-    static limb_type eval_add_n(      limb_type*           r,
-                                  const limb_type*         u,
-                                  const limb_type*         v,
-                                  const std::uint_fast32_t count,
-                                  const limb_type          carry_in = 0U)
+    static limb_type eval_add_n(      limb_type*         r,
+                                const limb_type*         u,
+                                const limb_type*         v,
+                                const std::uint_fast32_t count,
+                                const limb_type          carry_in = 0U)
     {
       limb_type carry_out = carry_in;
 
@@ -1766,7 +1769,7 @@
                                                 const std::uint_fast32_t n,
                                                       limb_type*         t)
     {
-      if(n <= 32U)
+      if(n <= 48U)
       {
         static_cast<void>(t);
 
@@ -1819,7 +1822,6 @@
               limb_type* r1 = r + nh;
               limb_type* r2 = r + n;
               limb_type* r3 = r + (n + nh);
-              limb_type* r4 = r + (n + n);
 
               limb_type* t0 = t + 0U;
               limb_type* t1 = t + nh;
@@ -1832,7 +1834,7 @@
         //   r -> t0
         eval_multiply_kara_n_by_n_to_2n(r2, a1, b1, nh, t0);
         eval_multiply_kara_n_by_n_to_2n(r0, a0, b0, nh, t0);
-        std::copy(r0, r4, t0);
+        std::copy(r0, r0 + (2U * n), t0);
 
         // Step 2
         //   r1 += a1*b1
@@ -1891,6 +1893,7 @@
       }
     }
 
+    #if 0
     static void eval_multiply_toomcook3(      limb_type*         r,
                                         const limb_type*         u,
                                         const limb_type*         v,
@@ -1912,6 +1915,7 @@
         // Cambridge University Press (2011).
 
         // TBD: Toom-Cook3
+        eval_multiply_kara_n_by_n_to_2n(r, u, v, n, t);
       }
     }
 
@@ -1932,8 +1936,10 @@
       else
       {
         // TBD: Toom-Cook4
+        eval_multiply_kara_n_by_n_to_2n(r, u, v, n, t);
       }
     }
+    #endif
 
     void eval_divide_knuth(const uintwide_t& other, uintwide_t* remainder)
     {
@@ -2320,12 +2326,12 @@
       // Implement pre-increment.
       std::uint_fast32_t i = 0U;
 
-      for( ; (i < (values.size() - 1U)) && (++values[i] == limb_type(0U)); ++i)
+      for( ; (i < std::uint_fast32_t(values.size() - 1U)) && (++values[i] == limb_type(0U)); ++i)
       {
         ;
       }
 
-      if(i == (values.size() - 1U))
+      if(i == std::uint_fast32_t(values.size() - 1U))
       {
         ++values[i];
       }
@@ -2336,12 +2342,12 @@
       // Implement pre-decrement.
       std::uint_fast32_t i = 0U;
 
-      for( ; (i < (values.size() - 1U)) && (values[i]-- == limb_type(0U)); ++i)
+      for( ; (i < std::uint_fast32_t(values.size() - 1U)) && (values[i]-- == limb_type(0U)); ++i)
       {
         ;
       }
 
-      if(i == (values.size() - 1U))
+      if(i == std::uint_fast32_t(values.size() - 1U))
       {
         --values[i];
       }
