a0bdc0bc02adb282910363e080632dec435a6fe7
John McFarlane <github@john.mcfarlane.name>
2021-05-01 17:17:20 +0100

    Support C++20 constant expressions
    When C++20 mode is enabled in compiler,
    many tests from examples/ will use constexpr for variables.
    This requires that library APIs they use are constant
    expressions.

--- examples/example002_shl_shr.cpp
+++ examples/example002_shl_shr.cpp
@@ -12,13 +12,13 @@ bool math::wide_integer::example002_shl_shr()
 {
   using uint256_t = math::wide_integer::uint256_t;
 
-  const uint256_t a("0xF4DF741DE58BCB2F37F18372026EF9CBCFC456CB80AF54D53BDEED78410065DE");
+  UINTWIDE_T_TEST_CONSTEXPR uint256_t a("0xF4DF741DE58BCB2F37F18372026EF9CBCFC456CB80AF54D53BDEED78410065DE");
 
-  const uint256_t c = (a << 67);
-  const uint256_t d = (a >> 79);
+  UINTWIDE_T_TEST_CONSTEXPR uint256_t c = (a << 67);
+  UINTWIDE_T_TEST_CONSTEXPR uint256_t d = (a >> 79);
 
-  const bool result_is_ok = (   (c == "0xBF8C1B901377CE5E7E22B65C057AA6A9DEF76BC208032EF00000000000000000")
-                             && (d == "0x1E9BEE83BCB17965E6FE306E404DDF3979F88AD97015E"));
+  UINTWIDE_T_TEST_CONSTEXPR bool result_is_ok = (   (c == "0xBF8C1B901377CE5E7E22B65C057AA6A9DEF76BC208032EF00000000000000000")
+                                                 && (d == "0x1E9BEE83BCB17965E6FE306E404DDF3979F88AD97015E"));
 
   return result_is_ok;
 }
