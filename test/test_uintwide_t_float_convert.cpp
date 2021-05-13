422c91a7fab1624cf722a6c37f695744868eeb5d
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-05-13 07:30:13 +0200

    Add more float construct test cases

--- test/test_uintwide_t_float_convert.cpp
+++ test/test_uintwide_t_float_convert.cpp
@@ -58,7 +58,8 @@ namespace
     using std::pow;
 
     const std::int32_t             p10 = dist_e10(engine_e10);
-    const local_builtin_float_type e10 = pow(local_builtin_float_type(10.0F), p10);
+    const local_builtin_float_type e10 = pow(local_builtin_float_type(10.0F),
+                                             local_builtin_float_type(p10));
 
     const local_builtin_float_type a = dist_man(engine_man) * e10;
 
@@ -106,6 +107,19 @@ bool math::wide_integer::test_uintwide_t_float_convert()
   engine_e10.seed(static_cast<typename std::linear_congruential_engine<std::uint32_t, 48271, 0, 2147483647>::result_type>(std::clock()));
 
   bool result_is_ok = true;
+
+  for(std::size_t i = 0U; i < 0x10000U; ++i)
+  {
+    const float f = get_random_float<float, -1, 27>();
+
+    boost_sint_type n_boost = boost_sint_type(f);
+    local_sint_type n_local = local_sint_type(f);
+
+    const std::string str_boost_signed = hexlexical_cast((boost_uint_type) n_boost);
+    const std::string str_local_signed = hexlexical_cast((local_uint_type) n_local);
+
+    result_is_ok &= (str_boost_signed == str_local_signed);
+  }
 
   for(std::size_t i = 0U; i < 0x10000U; ++i)
   {
