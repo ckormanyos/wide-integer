ca0dc733c6dfad212c8cfd2a145d11dc883cf239
ckormanyos <ckormanyos@yahoo.com>
2025-06-09 10:06:07 +0200

    Restore a missing Boost header

--- test/test_uintwide_t_boost_backend_via_test_arithmetic.cpp
+++ test/test_uintwide_t_boost_backend_via_test_arithmetic.cpp
@@ -44,20 +44,25 @@
 #include <test/test_arithmetic.hpp>
 #include <test/test_uintwide_t.h>
 
+#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/uintwide_t_backend.hpp>
 
 // cd /mnt/c/Users/ckorm/Documents/Ks/PC_Software/NumericalPrograms/ExtendedNumberTypes/wide_integer
-// g++ -march=native -mtune=native -O2 -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion -Wshadow -std=c++20 -DWIDE_INTEGER_HAS_LIMB_TYPE_UINT64 -I. -I/mnt/c/boost/boost_1_88_0 test/test_uintwide_t_boost_backend_via_test_arithmetic.cpp -o test_uintwide_t_boost_backend_via_test_arithmetic
-// ./test_uintwide_t_boost_backend_via_test_arithmetic
+// g++ -march=native -mtune=native -O2 -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion -Wshadow -std=c++20 -I. -I/mnt/c/boost/boost_1_88_0 test/test_uintwide_t_boost_backend_via_test_arithmetic.cpp -o test_uintwide_t_boost_backend_via_test_arithmetic.exe
+// ./test_uintwide_t_boost_backend_via_test_arithmetic.exe
 
 auto main() -> int
 {
-  using local_big_uint_backend_type =
-    boost::multiprecision::uintwide_t_backend<static_cast<math::wide_integer::size_t>(UINT32_C(1024)), std::uint8_t, std::allocator<void>>;
+  #if defined(WIDE_INTEGER_NAMESPACE)
+  using local_size_type = WIDE_INTEGER_NAMESPACE::math::wide_integer::size_t;
+  #else
+  using local_size_type = ::math::wide_integer::size_t;
+  #endif
 
-  using local_big_uint_type =
-    boost::multiprecision::number<local_big_uint_backend_type,
-                                  boost::multiprecision::et_off>;
+  using local_big_uint_backend_type =
+    ::boost::multiprecision::uintwide_t_backend<local_size_type { UINT32_C(1024) }, std::uint32_t, std::allocator<void>>;
+
+  using local_big_uint_type = ::boost::multiprecision::number<local_big_uint_backend_type, boost::multiprecision::et_off>;
 
   test<local_big_uint_type>();
 
